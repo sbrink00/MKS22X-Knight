9e9f1dc935a60e94eb79284260e4e2ca3d5dd358
sbrink00 <sbrink00@stuy.edu>
2019-02-13 09:55:01 -0500

    created file and wrote constructor that initializes all rows to zero

---
+++ KnightBoard.java
@@ -0,0 +1,12 @@
+public class KnightBoard{
+  public static void main(String[]args){
+
+  }
+
+  private int[][] board;
+
+  public KnightBoard(int startingRows, int startingCols){
+    board = new int[startingRows][startingCols];
+  }
+
+}
